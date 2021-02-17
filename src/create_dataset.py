110465d81465729fc6a37afc3955bf085b1781d3
myscience <muratore.paolo95@gmail.com>
2021-02-17 19:58:54 +0100

    Update create_dataset: characters are not stretched and are now centered in words

--- src/create_dataset.py
+++ src/create_dataset.py
@@ -25,14 +25,19 @@ word_idx = [[1,2,4],
 hfd_idx = [1, 2, 3]
 lfd_idx  = [5, 6, 4]
 
-# Impose common size for consistency 
-# NOTE: This is needed for a ANN which needs consistent input dimensions
-chr_size = (620, 574)
-wrd_size = (1860, 574)
-
 # Import the characters from file
 tot = 24
-chars = np.array ([Image.open (chr_path + f'{idx}.jpg').resize(chr_size) for idx in range(1, tot)], dtype = np.object)
+chars = np.array ([Image.open (chr_path + f'{idx}.jpg') for idx in range(1, tot)], dtype = np.object)
+
+# Here we identify the largest character width and height
+shapes = np.array ([np.shape(char)[:2] for char in chars])
+
+max_h = max (shapes[:, 0])
+max_w = max (shapes[:, 1])
+
+# We can now compute appropriate word size
+# NOTE: This is needed for a ANN which needs consistent input dimensions
+wrd_size = (3 * max_w, max_h)
 
 dataset = f'../data/dataset{0}/'
 chr_perm = np.random.permutation (tot - 1)
@@ -56,14 +61,25 @@ def white2alpha(img, thr = (100, 100, 100, 100)):
 for i, chr_imgs in enumerate (chr_imgs_set):
     word = Image.new ('RGBA', wrd_size)
 
+    # Get the character width for appropriate positioning
+    chrs_h = [np.shape (chr_imgs)[0] for chr_imgs in chr_imgs]
+    chrs_w = [np.shape (chr_imgs)[1] for chr_imgs in chr_imgs]
+
+    wrd_w = sum (chrs_w)
+    pad = (max_w * 3 - wrd_w) // 2
+
+    # dims = np.array ([0] + [np.shape (chr_img)[1] for chr_img in chr_imgs])
+    dims = [(w, h) for w, h in zip(np.cumsum ([pad] + chrs_w)[:-1], chrs_h)]
+
     # Here we assemble the word char by char
-    [word.paste (chr_img, at) for chr_img, at in zip(chr_imgs, [(0, 0), (620, 0), (1240, 0)])]
+    [word.paste (chr_img, at) for chr_img, at in zip(chr_imgs, ((w, max_h - h) for w, h in dims))]
 
     # Convert white pixels to alpha
     word = white2alpha(word)
 
     # Here we save the word in current dataset
     word.save(dataset + f'word_{i}.png')
+
 
 # Create & Store the high-low frequency deviants
 hfd_word = Image.new('RGBA', wrd_size)    
@@ -72,8 +88,25 @@ lfd_word = Image.new('RGBA', wrd_size)
 hfd_chars = chars[chr_perm[hfd_idx]]
 lfd_chars = chars[chr_perm[lfd_idx]]
 
-[hfd_word.paste (chr_img, at) for chr_img, at in zip(hfd_chars, [(0, 0), (620, 0), (1240, 0)])]
-[lfd_word.paste (chr_img, at) for chr_img, at in zip(lfd_chars, [(0, 0), (620, 0), (1240, 0)])]
+# Get the character width for appropriate positioning
+hfd_chrs_h = [np.shape (chr_imgs)[0] for chr_imgs in hfd_chars]
+hfd_chrs_w = [np.shape (chr_imgs)[1] for chr_imgs in hfd_chars]
+
+lfd_chrs_h = [np.shape (chr_imgs)[0] for chr_imgs in lfd_chars]
+lfd_chrs_w = [np.shape (chr_imgs)[1] for chr_imgs in lfd_chars]
+
+hfd_wrd_w = sum (hfd_chrs_w)
+hfd_pad = (max_w * 3 - hfd_wrd_w) // 2
+
+lfd_wrd_w = sum (lfd_chrs_w)
+lfd_pad = (max_w * 3 - lfd_wrd_w) // 2
+
+# dims = np.array ([0] + [np.shape (chr_img)[1] for chr_img in chr_imgs])
+hfd_dims = [(w, h) for w, h in zip(np.cumsum ([hfd_pad] + hfd_chrs_w)[:-1], hfd_chrs_h)]
+lfd_dims = [(w, h) for w, h in zip(np.cumsum ([lfd_pad] + lfd_chrs_w)[:-1], lfd_chrs_h)]
+
+[hfd_word.paste (chr_img, at) for chr_img, at in zip(hfd_chars, ((w, max_h - h) for w, h in hfd_dims))]
+[lfd_word.paste (chr_img, at) for chr_img, at in zip(lfd_chars, ((w, max_h - h) for w, h in lfd_dims))]
 
 # Convert white pixels to alpha
 hfd_word = white2alpha (hfd_word)
@@ -81,6 +114,3 @@ lfd_word = white2alpha (lfd_word)
 
 hfd_word.save(dataset + f'hfd_word.png')
 lfd_word.save(dataset + f'lfd_word.png')
-
-
-
